098c98f86475d1c3578bd23a92375c094c893cbb
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-12 12:59:54 +0200

    gets!

--- routes/cliente.js
+++ routes/cliente.js
@@ -4,6 +4,54 @@ let app = express();
 
 let Cliente = require('../models/cliente');
 
+app.get('/', (req, res) => {
+
+    Cliente.find({}, (error, clientes) => {
+        if (error) {
+            return res.status(400).json({
+                error: error
+            })
+        }
+
+        res.status(200).json({
+            clientes: clientes
+        })
+
+    })
+})
+
+app.get('/:_id', (req, res) => {
+
+    Cliente.findById(req.params._id, (error, cliente) => {
+        if (error) {
+            return res.status(400).json({
+                error: error
+            })
+        }
+
+        res.status(200).json({
+            cliente: cliente
+        })
+
+    })
+})
+
+app.get('/cif/:cif', (req, res) => {
+
+    Cliente.find({cif: req.params.cif}, (error, cliente) => {
+        if (error) {
+            return res.status(400).json({
+                error: error
+            })
+        }
+
+        res.status(200).json({
+            cliente: cliente
+        })
+
+    })
+})
+
 app.post('/', (req, res) => {
 
     let cliente = new Cliente({
